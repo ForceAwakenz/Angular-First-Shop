e597ebf0a2b791d2a1d41b35c701ee5f1d22d42d
ForceAwakenz <djimeilsobachka@gmail.com>
2021-10-10 21:44:42 +0300

    added empty admin page, routing and module

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { AdminComponent } from './admin.component';
+import { AppRoutingModule } from '../app-routing.module';
+
+@NgModule({
+  declarations: [AdminComponent],
+  imports: [AppRoutingModule],
+})
+export class AdminModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AdminModule } from './admin/admin.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
 import { ShowcaseComponent } from './showcase/showcase.component';
+// import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
-    ShowcaseComponent
+    ShowcaseComponent,
+    // AdminComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, AdminModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
